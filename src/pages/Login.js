c186da0f744aaef32290e0c68cda88c47444d48e
Jhon Santana <jhonsstn@gmail.com>
2022-03-31 15:13:21 -0300

    feat:requisitos 1 e 2 completos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,99 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { userLogin } from '../actions';
+
+const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{3})+$/;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.isEmailValid = this.isEmailValid.bind(this);
+    this.isPasswordValid = this.isPasswordValid.bind(this);
+    this.isSubmitDisabled = this.isSubmitDisabled.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.isSubmitDisabled(),
+    );
+  }
+
+  isEmailValid() {
+    const { email } = this.state;
+    return EMAIL_REGEX.test(email);
+  }
+
+  isPasswordValid() {
+    const { password } = this.state;
+    const minPassLength = 6;
+    return password.length >= minPassLength;
+  }
+
+  isSubmitDisabled() {
+    this.setState({
+      disabled: !(this.isEmailValid() && this.isPasswordValid()),
+    });
+  }
+
+  handleSubmit() {
+    const { login, history } = this.props;
+    login(this.state);
+    history.push('/carteira');
+  }
+
   render() {
-    return <h1>Trybe Wallet</h1>;
+    const { email, password, disabled } = this.state;
+    return (
+      <div>
+        <h1>Trybe Wallet</h1>
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="button"
+          value="Entrar"
+          onClick={ this.handleSubmit }
+          disabled={ disabled }
+        />
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  login: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (state) => dispatch(userLogin(state)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_LOGIN } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  const { payload } = action;
+  switch (action.type) {
+  case USER_LOGIN:
+    return { email: payload.email };
+  default:
+    return state;
+  }
+};
+
+export default user;
